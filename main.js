6f11e6d214aa21b159a91987e5ca9cd98eaf34f5
James Petran <jm.ptrn@gmail.com>
2021-12-09 14:01:55 -0600

    input& button

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-$(document).ready(readyNow);
+$(document).ready( readyNow);
 
 function readyNow() {
       console.log('JQ');
-      
+
+      $('#top').append(`
+      <h1>
+            Hi there this is my jQuery test page and its cool
+      </h1>
+      <h5>this is h5 text</h5>
+      `)
+      $(`plantButton`).on('click', plantAdder)
+
 
   } // end readyNow
+
+function plantAdder() {
+      let inputBox = $('#plantName').val();
+      console.log(inputBox);
+}
